c899ce0f3cd3f87219b00f976865e82f1189a2d2
Luiz Carlos <luiz.manhani@gmail.com>
2016-01-24 23:24:26 -0200

    adicionando o bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+elixir(function(mix) { 
+    var bootstrapPath = 'node_modules/bootstrap-sass/assets'; 
+    mix.sass('app.scss').copy(bootstrapPath + '/fonts', 'public/fonts').copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js'); 
 });
---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Estudo Laravel</title>
+    <link rel="stylesheet" type="text/css" href="css\app.css"/>
+</head>
+<body>
+    <header>
+        <a id="logo" href="#sobre">LCMJR</a>
+        <nav></nav>
+    </header>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
